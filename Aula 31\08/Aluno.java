0ccb91197c5505e0086aa524d8167f79e0e8a2ca
hifeche <117675004+hifeche@users.noreply.github.com>
2026-08-31 20:40:28 -0300

    Add files via upload

---
+++ Aula 31\08/Aluno.java
@@ -0,0 +1,38 @@
+public class Aluno {
+    private String nome;
+    private String matricula;
+    private float nota1;
+    private float nota2;
+    private float nota3;
+    private float nota4;
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public void setNota1(float nota1) {
+        this.nota1 = nota1;
+    }
+
+    public void setNota2(float nota2) {
+        this.nota2 = nota2;
+    }
+
+    public void setNota3(float nota3) {
+        this.nota3 = nota3;
+    }
+
+    public void setNota4(float nota4) {
+        this.nota4 = nota4;
+    }
+
+    
+}
