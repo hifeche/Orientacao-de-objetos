dcf41eb070addc0392dd71901d3522b81ae50b7e
hifeche <117675004+hifeche@users.noreply.github.com>
2026-09-16 19:40:18 -0300

    Add files via upload

---
+++ Aula 14\09/4_vetores_completo/Trabalho/companhia.java
@@ -0,0 +1,68 @@
+public class companhia {
+    private String nome;
+    private float cnpj;
+    private String pais;
+    private float telefone;
+    private String email;
+    private String endereco;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public float getCnpj() {
+        return cnpj;
+    }
+
+
+    public void setCnpj(float cnpj) {
+        this.cnpj = cnpj;
+    }
+
+
+    public String getPais() {
+        return pais;
+    }
+
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+
+    public float getTelefone() {
+        return telefone;
+    }
+
+
+    public void setTelefone(float telefone) {
+        this.telefone = telefone;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
--- Aula 14\09/4_vetores_completo/Trabalho/voo.java
+++ Aula 14\09/4_vetores_completo/Trabalho/voo.java
@@ -1,3 +1,114 @@
 public class voo {
-    private String 
+    private float id;
+    private float numero;
+    private String companhia;
+    private String origem;
+    private String destino;
+    private float data;
+    private float hora_saida;
+    private float hora_chagada;
+    private String status;
+    private String pilotos;
+    
+    
+    
+    public float getId() {
+        return id;
+    }
+    
+    
+    public void setId(float id) {
+        this.id = id;
+    }
+    
+    
+    public float getNumero() {
+        return numero;
+    }
+    
+    
+    public void setNumero(float numero) {
+        this.numero = numero;
+    }
+    
+    
+    public String getCompanhia() {
+        return companhia;
+    }
+    
+    
+    public void setCompanhia(String companhia) {
+        this.companhia = companhia;
+    }
+    
+    
+    public String getOrigem() {
+        return origem;
+    }
+
+
+    public void setOrigem(String origem) {
+        this.origem = origem;
+    }
+    
+    
+    public String getDestino() {
+        return destino;
+    }
+    
+    
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+    
+    
+    public float getData() {
+        return data;
+    }
+    
+    
+    public void setData(float data) {
+        this.data = data;
+    }
+    
+    
+    public float getHora_saida() {
+        return hora_saida;
+    }
+    
+    
+    public void setHora_saida(float hora_saida) {
+        this.hora_saida = hora_saida;
+    }
+    
+    
+    public float getHora_chagada() {
+        return hora_chagada;
+    }
+    
+    
+    public void setHora_chagada(float hora_chagada) {
+        this.hora_chagada = hora_chagada;
+    }
+    
+    
+    public String getStatus() {
+        return status;
+    }
+    
+    
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    
+    public String getPilotos() {
+        return pilotos;
+    }
+
+
+    public void setPilotos(String pilotos) {
+        this.pilotos = pilotos;
+    }
+
 }
