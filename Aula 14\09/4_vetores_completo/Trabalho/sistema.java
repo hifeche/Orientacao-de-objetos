2ad1fc246df8480ed7c6d48b141f7a8d4ed8a0e8
hifeche <117675004+hifeche@users.noreply.github.com>
2026-09-21 15:27:30 -0300

    Implement main menu for flight registration system

---
+++ Aula 14\09/4_vetores_completo/Trabalho/sistema.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        // Criamos uma companhia
+        companhia companhia = new companhia();
+
+        int opcao;
+
+        do {
+
+            System.out.println("\n===== MENU =====");
+            System.out.println("1 - Cadastrar voo");
+            System.out.println("2 - Listar voos");
+            System.out.println("3 - Consultar voo");
+            System.out.println("0 - Sair");
+            System.out.print("Escolha uma opcao: ");
+
+            opcao = teclado.nextInt();
+
+            switch (opcao) {
+
+                case 1:
+                    System.out.println("Cadastrar voo");
+                    break;
+
+                case 2:
+                    System.out.println("Listar voos");
+                    break;
+
+                case 3:
+                    System.out.println("Consultar voo");
+                    break;
+
+                case 0:
+                    System.out.println("Programa encerrado.");
+                    break;
+
+                default:
+                    System.out.println("Opcao invalida!");
+            }
+
+        } while (opcao != 0);
+
+        teclado.close();
+    }
+}
